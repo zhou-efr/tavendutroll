a281f65a1c98d3bc958d2f6737da52ae81703948
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-01-16 20:45:53 +0100

    office & readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Taverne du troll
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+![](https://img.shields.io/badge/front-react-blue) ![](https://img.shields.io/badge/front-tailwind-blue) ![](https://img.shields.io/badge/api-express-yellow)
 
 ## Available Scripts
 In the project directory, you can run:
@@ -7,3 +7,8 @@ In the project directory, you can run:
 ### `npm start`
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+### `cd taverndutrollapi && node index.js`
+Runs the backend api. \
+Open [http://localhost:8080](http://localhost:8080) to view it in your browser. \
+/!\ You need to preset a mysql database before use it.
--- src/Component/Pages/Office.js
+++ src/Component/Pages/Office.js
@@ -1,7 +1,57 @@
+import prez from "../../images/prew.png";
+import panda from "../../images/panda.jpg";
+
 export const Office = (props) => {
   return (
-      <div className={'h-3/4'}>
-          <h1 className="text-3xl font-bold">office</h1>
+      <div className={''}>
+          <div className="flex flex-row justify-around">
+              <div className="flex flex-row justify-center p-10">
+                  <img className={"w-40 h-40 object-cover rounded-full"} alt={"post"} src={panda}/>
+                  <div className={"flex flex-col ml-9 justify-center items-start"}>
+                      <h2 className={"text-xl font-bold"}>
+                          Présidente
+                      </h2>
+                      <p className={"text-base"}>
+                          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ration
+                      </p>
+                  </div>
+              </div>
+              <div className="flex flex-row justify-center p-10">
+                  <div className={"flex flex-col ml-9 w-3/4 justify-center items-start"}>
+                      <h2 className={"text-xl font-bold"}>
+                          Secrétaire
+                      </h2>
+                      <p className={"text-base"}>
+                          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ration
+                      </p>
+                  </div>
+                  <img className={"w-40 h-40 object-cover rounded-full"} alt={"post"} src={panda}/>
+              </div>
+          </div>
+          <div className="flex flex-row justify-around">
+              <div className="flex flex-row justify-center p-10">
+                  <div className={"flex flex-col ml-9 justify-center items-start"}>
+                      <h2 className={"text-xl font-bold"}>
+                          Trésorier
+                      </h2>
+                      <p className={"text-base"}>
+                          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ration
+                      </p>
+                  </div>
+                  <img className={"w-40 h-40 object-cover rounded-full"} alt={"post"} src={panda}/>
+              </div>
+              <div className="flex flex-row justify-center p-10">
+                  <img className={"w-40 h-40 object-cover rounded-full"} alt={"post"} src={panda}/>
+                  <div className={"flex flex-col ml-9 w-3/4 justify-center items-start"}>
+                      <h2 className={"text-xl font-bold"}>
+                          Vice-Président
+                      </h2>
+                      <p className={"text-base"}>
+                          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ration
+                      </p>
+                  </div>
+              </div>
+          </div>
       </div>
   );
 }
