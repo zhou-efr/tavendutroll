29c55ab3d547b8aa21438ab6006c01a3f884a566
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-02-24 11:48:56 +0800

    azure function api - 7

--- tdtAPI/publication/index.js
+++ tdtAPI/publication/index.js
@@ -1,7 +1,5 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the API",
-        "context": context,
-        "req": req
+        text: "Hello from the API"
     });
 }
