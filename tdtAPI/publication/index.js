437e9c988f424066568706bea5bf407a2fe97aeb
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-02-26 00:45:03 +0800

    azure function api - 12

--- tdtAPI/publication/index.js
+++ tdtAPI/publication/index.js
@@ -1,28 +1,5 @@
-const {api_item} = require("./Common");
-const {Request} = require("tedious");
-const {error_res, output_res, connection, rows_to_json} = require("../Common");
-const post_sql_func = (req) => {
-    return `
-        insert into publication (
-            name,
-            author,
-            pole,
-            description,
-            content,
-            imageUrl
-        )
-        VALUES (
-            '${req.body['name']}',
-            '${req.body['author']}',
-            '${req.body['pole']}',
-            '${req.body['description']}',
-            '${req.body['content']}',
-            '${req.body['thumbnail']}'
-        )`;
-}
-
 module.exports = async function (context, req) {
-    context.res.status(200).json({
+    context.res.json({
         text: "Hello from the API"
     });
 };
