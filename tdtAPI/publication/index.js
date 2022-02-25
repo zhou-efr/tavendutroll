975d5dbaa8c90a3ffaea1ba17ace3781536e6bff
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-02-26 03:01:15 +0800

    azure function api - 23

--- tdtAPI/publication/index.js
+++ tdtAPI/publication/index.js
@@ -1,8 +1,8 @@
 const {output_res, connection} = require("../Common");
 module.exports = async function (context, req) {
-    let sql = `select * from publication`;
+    // let sql = `select * from publication`;
     // output_res(context, sql, connection);
     context.res.json({
-        "panda": "panda"
+        panda: "panda"
     });
 };
