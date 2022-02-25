d521146f774ba6a82b73687bf41959481a197aaa
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-02-26 04:06:04 +0800

    azure function api - 30

--- tdtAPI/publication/index.js
+++ tdtAPI/publication/index.js
@@ -1,4 +1,23 @@
-const {api_item} = require("./Common");
+const { Connection, Request} = require("tedious");
+const config = {
+    authentication: {
+        options: {
+            userName: process.env.tdtDatabaseUser,
+            password: process.env.tdtPassword
+        },
+        type: "default"
+    },
+    server: process.env.tdtDatabaseServer,
+    options: {
+        database: process.env.tdtDatabaseName,
+        encrypt: true,
+        port: parseInt(process.env.tdtDatabasePort),
+        rowCollectionOnRequestCompletion: true,
+    }
+};
+const connection = new Connection(config);
+connection.connect();
+
 const post_sql_func = (req) => {
     return `
         insert into publication (
@@ -18,7 +37,27 @@ const post_sql_func = (req) => {
             '${req.body['thumbnail']}'
         )`;
 }
+const rows_to_json = (rows) => {
+    let output = [];
+    for (const rowsKey in rows) {
+        let row = {};
+        for (const rowKey in rows[rowsKey]) {
+            row[rows[rowsKey][rowKey]["metadata"]["colName"]] = rows[rowsKey][rowKey]["value"];
+        }
+        output.push(row);
+    }
+    return output;
+};
 
 module.exports = async function (context, req) {
-    api_item("publication", post_sql_func, context, req);
+    connection.execSql(new Request("select * from publication", (err, rowCount, rows) => {
+        if (err) {
+            context.res.json({
+                error: err
+            });
+        } else {
+            let response = rows_to_json(rows);
+            context.res.json(response);
+        }
+    }));
 };
