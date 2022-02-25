dd2bfd673871fde137a4d54530cb805f793d8ff3
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-02-26 02:09:33 +0800

    azure function api - 18

--- tdtAPI/Common.js
+++ tdtAPI/Common.js
@@ -37,44 +37,45 @@ const error_res = (res, err) => {
     }
 }
 const output_res = (res, sql, connection) => {
-    res.json({panda: "ours polaire"});
-    connection.execSql(new Request(sql, (err, rowCount, rows) => {
-        if (err) {
-            error_res(res, err)
-        } else {
-            let response = rows_to_json(rows);
-            res.json(response);
-        }
-    }));
+    res.json({
+        "panda": "panda"
+    });
+    // connection.execSql(new Request(sql, (err, rowCount, rows) => {
+    //     if (err) {
+    //         error_res(res, err)
+    //     } else {
+    //         let response = rows_to_json(rows);
+    //         res.json(response);
+    //     }
+    // }));
 };
 const api_item = async (table, post_sql_func, context, req) => {
     let sql = `select * from ${table}`;
-    output_res(context.res, sql, connection);
-    // let id = req.query.id || (req.body && req.body.id);
-    // console.log("get");
-    // switch (req.method) {
-    //     case "POST":
-    //         let post_sql = post_sql_func(req)
-    //         let post_request = new Request(post_sql, (err) => error_res(context.res, err));
-    //         post_request.on('requestCompleted', () => output_res(context.res, sql, connection));
-    //         connection.execSql(post_request);
-    //         break;
-    //     case "DELETE":
-    //         if (id){
-    //             let del_sql = `delete from ${table} where id = ${id}`;
-    //             let del_request = new Request(del_sql, (err) => error_res(context.res, err));
-    //             del_request.on('requestCompleted', () => output_res(context.res, sql, connection));
-    //             connection.execSql(del_request);
-    //         } else {
-    //             error_res(context.res, "no id given")
-    //         }
-    //         break;
-    //     default: // GET
-    //         if (id) {
-    //             sql = `select * from ${table} where id = ${id}`;
-    //         }
-    //         output_res(context.res, sql, connection)
-    // }
+    let id = req.query.id || (req.body && req.body.id);
+    console.log("get");
+    switch (req.method) {
+        case "POST":
+            let post_sql = post_sql_func(req)
+            let post_request = new Request(post_sql, (err) => error_res(context.res, err));
+            post_request.on('requestCompleted', () => output_res(context.res, sql, connection));
+            connection.execSql(post_request);
+            break;
+        case "DELETE":
+            if (id){
+                let del_sql = `delete from ${table} where id = ${id}`;
+                let del_request = new Request(del_sql, (err) => error_res(context.res, err));
+                del_request.on('requestCompleted', () => output_res(context.res, sql, connection));
+                connection.execSql(del_request);
+            } else {
+                error_res(context.res, "no id given")
+            }
+            break;
+        default: // GET
+            if (id) {
+                sql = `select * from ${table} where id = ${id}`;
+            }
+            output_res(context.res, sql, connection)
+    }
 }
 
 module.exports = {
--- tdtAPI/publication/index.js
+++ tdtAPI/publication/index.js
@@ -1,22 +1,4 @@
-const {api_item} = require("./Common");
-const post_sql_func = (req) => {
-    return `
-        insert into publication (
-            name,
-            author,
-            pole,
-            description,
-            content,
-            imageUrl
-        )
-        VALUES (
-            '${req.body['name']}',
-            '${req.body['author']}',
-            '${req.body['pole']}',
-            '${req.body['description']}',
-            '${req.body['content']}',
-            '${req.body['thumbnail']}'
-        )`;
-}
-
-module.exports = async (context, req) => await api_item("publication", post_sql_func, context, req);
+module.exports = async function (context, req) {
+    let sql = `select * from publication`;
+    output_res(context.res, sql, connection);
+};
