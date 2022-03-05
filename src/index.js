888826a048ab3973d1b95f3778777c2808275791
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-03-06 03:35:23 +0800

    meta data - 1

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logo from '../images/logo.png' ;
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <tiltle>La taverne du troll</tiltle>
+    <meta property="og:title" content="La taverne du troll"/>
+    <meta name="twitter:title" content="La taverne du troll"/>
+
+    <meta name="description" content="Association de jeux d'EFREI Paris."/>
+    <meta property="og:description" content="Association de jeux d'EFREI Paris."/>
+    <meta name="twitter:description" content="Your Description"/>
+
+    <meta property="og:image" content={logo}/>
+    <meta name="twitter:image" content={logo}/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
