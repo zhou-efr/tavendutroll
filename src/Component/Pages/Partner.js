40a742f68fc6333859fe3777cbd9d02fa3f1c4dd
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2022-04-02 04:16:47 +0800

    no reffer - 1

--- src/Component/Pages/Partner.js
+++ src/Component/Pages/Partner.js
@@ -6,7 +6,7 @@ export const Partner = () => {
     return (
         <div className={'h-screen-5/4'}>
             <div className={'h-screen-1/2 w-screen bg-cover bg-wooden-header flex flex-row justify-center gap-32 items-center pt-96'}>
-                <a href={"https://www.starplayer.fr/"} target="_blank">
+                <a href={"https://www.starplayer.fr/"} target="_blank" rel="noreferrer">
                     <div className={"flex flex-col items-center w-80 mt-36"}>
                         <img className={"rounded-full"} src={logoStarplayer} alt={"Starplayer"}/>
                         <h3 className={"text-3xl mt-3 font-bold text-center hover:underline"}>
@@ -17,7 +17,7 @@ export const Partner = () => {
                         </p>
                     </div>
                 </a>
-                <a href={"https://www.loufoque.fr/"} target="_blank">
+                <a href={"https://www.loufoque.fr/"} target="_blank" rel="noreferrer">
                     <div className={"flex flex-col items-center mt-36 w-80"}>
                         <img className={"rounded-full w-80"} src={logoLoufoque} alt={"Le Loufoque"}/>
                         <h3 className={"text-3xl mt-3 font-bold text-center hover:underline"}>
